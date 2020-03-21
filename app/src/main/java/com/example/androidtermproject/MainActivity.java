cd4a7f87d53b660eb1b2955554b32c74c1c8e1d3
amrithasusanjacob <amrithasusanjacob@gmail.com>
2020-03-21 19:54:20 -0400

    Demo commit

--- app/src/main/java/com/example/androidtermproject/MainActivity.java
+++ app/src/main/java/com/example/androidtermproject/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String APP_NAME = "Hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
