d450774747cc1a0371fffad4dac5219cb335a00a
Febin13 <febinbabujohn@gmail.com>
2020-03-25 19:56:39 -0400

    Started with Employee Registration activity

---
+++ app/src/main/java/com/example/androidtermproject/EmpRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidtermproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmpRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emp_registration);
+    }
+}
