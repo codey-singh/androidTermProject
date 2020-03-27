7ee46ad840e30a7e296b7711c46c7a2418fc3f30
bhajanpreet <bhajanpreet25@gmail.com>
2020-03-27 12:42:13 -0400

    Added a button to view the reg form

--- app/src/main/java/com/example/androidtermproject/MainActivity.java
+++ app/src/main/java/com/example/androidtermproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidtermproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String demo = "Hello demo";
+    }
+
+    public void goToReg(View view) {
+        Intent intent = new Intent(this, EmpRegistrationActivity.class);
+        startActivity(intent);
     }
 }
