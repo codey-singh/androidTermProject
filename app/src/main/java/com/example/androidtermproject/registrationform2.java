4071de610468e979f8ece6ccb5b9d8120a8bfd1c
Febin13 <febinbabujohn@gmail.com>
2020-03-30 23:36:27 -0400

    Commit after creating Form in Linear Layout

---
+++ app/src/main/java/com/example/androidtermproject/registrationform2.java
@@ -0,0 +1,14 @@
+package com.example.androidtermproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrationform2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrationform2);
+    }
+}
