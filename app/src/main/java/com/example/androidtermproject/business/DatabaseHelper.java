416b91c2771eaef3f2d259518a1c6b5e25f9f048
bhajanpreet <bhajanpreet25@gmail.com>
2020-03-31 00:55:15 -0400

    Added functionality to get employees insert employee and insert vehicle.

--- app/src/main/java/com/example/androidtermproject/business/DatabaseHelper.java
+++ app/src/main/java/com/example/androidtermproject/business/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.androidtermproject.business;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,9 +8,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.androidtermproject.models.Car;
 import com.example.androidtermproject.models.IEmployee;
 import com.example.androidtermproject.models.IVehicle;
 import com.example.androidtermproject.models.Manager;
+import com.example.androidtermproject.models.Motorcycle;
 import com.example.androidtermproject.models.Programmer;
 import com.example.androidtermproject.models.Tester;
 
@@ -58,11 +61,43 @@ public class DatabaseHelper extends SQLiteOpenHelper implements IDataService {
 
     @Override
     public boolean addEmployee(IEmployee employee) {
-        return false;
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        switch (employee.getRole()){
+            case "Programmer":
+                values.put("id", employee.getEmpId());
+                values.put("name", ((Programmer) employee).getName());
+                values.put("age", ((Programmer) employee).getAge());
+                values.put("birthYear", ((Programmer) employee).getBirthYear());
+                values.put("monthlySalary", ((Programmer) employee).getMonthlySalary());
+                values.put("rate", ((Programmer) employee).getRate());
+                values.put("nbProjects", ((Programmer) employee).getNbProjects());
+                break;
+            case "Tester":
+                values.put("id", employee.getEmpId());
+                values.put("name", ((Tester) employee).getName());
+                values.put("age", ((Tester) employee).getAge());
+                values.put("birthYear", ((Tester) employee).getBirthYear());
+                values.put("monthlySalary", ((Tester) employee).getMonthlySalary());
+                values.put("rate", ((Tester) employee).getRate());
+                values.put("nbBugs", ((Tester) employee).getNbBugs());
+                break;
+            case "Manager":
+                values.put("id", employee.getEmpId());
+                values.put("name", ((Manager) employee).getName());
+                values.put("age", ((Manager) employee).getAge());
+                values.put("birthYear", ((Manager) employee).getBirthYear());
+                values.put("monthlySalary", ((Manager) employee).getMonthlySalary());
+                values.put("rate", ((Manager) employee).getRate());
+                values.put("nbClients", ((Manager) employee).getNbClients());
+                break;
+        }
+        return db.insert("Employees", null, values) != -1;
     }
 
     @Override
     public boolean removeEmployee(int id) {
+
         return false;
     }
 
@@ -73,7 +108,30 @@ public class DatabaseHelper extends SQLiteOpenHelper implements IDataService {
 
     @Override
     public boolean addVehicle(IVehicle vehicle) {
-        return false;
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        switch (vehicle.getVehicleType()) {
+            case "Car":
+                values.put("id", vehicle.getVehicleId());//int id, String make, String plate, String color, String category, String type, int belongsTo
+                values.put("make", ((Car)vehicle).getMake());
+                values.put("plate", ((Car) vehicle).getPlate());
+                values.put("color", ((Car) vehicle).getColor());
+                values.put("category", ((Car) vehicle).getCategory());
+                values.put("type", ((Car) vehicle).getType());
+                values.put("belongsTo", ((Car) vehicle).getBelongsTo());
+                break;
+            case "Motorcycle":
+                values.put("id", vehicle.getVehicleId());//int id, String make, String plate, String color, String category, String type, int belongsTo
+                values.put("make", ((Motorcycle)vehicle).getMake());
+                values.put("plate", ((Motorcycle) vehicle).getPlate());
+                values.put("color", ((Motorcycle) vehicle).getColor());
+                values.put("category", ((Motorcycle) vehicle).getCategory());
+                values.put("type", ((Motorcycle) vehicle).isHasSideCar());
+                values.put("belongsTo", ((Motorcycle) vehicle).getBelongsTo());
+                break;
+        }
+
+        return db.insert("Vehicles", null, values) != -1;
     }
 
     @Override
