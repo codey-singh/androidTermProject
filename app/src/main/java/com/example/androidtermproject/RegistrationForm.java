f95901a33a7d15107e9e27a3282c20bb2c293f3b
Febin13 <febinbabujohn@gmail.com>
2020-03-31 20:12:51 -0400

    Commit after properly creating new Registration Form

--- app/src/main/java/com/example/androidtermproject/RegistrationForm.java
+++ app/src/main/java/com/example/androidtermproject/RegistrationForm.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class registrationform2 extends AppCompatActivity {
+public class RegistrationForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrationform2);
+        setContentView(R.layout.activity_registration_form);
     }
 }
