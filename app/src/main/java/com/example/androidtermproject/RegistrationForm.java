e5b5dc7f713f6f48f2a237b6429509ea6de23eb7
amrithasusanjacob <amrithasusanjacob@gmail.com>
2020-04-12 05:03:38 -0400

    added validation to firstname field

--- app/src/main/java/com/example/androidtermproject/RegistrationForm.java
+++ app/src/main/java/com/example/androidtermproject/RegistrationForm.java
@@ -1,14 +1,30 @@
 package com.example.androidtermproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class RegistrationForm extends AppCompatActivity {
+    //Variable declaration
+    EditText e_fname, e_lname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+        e_fname = findViewById(R.id.FirstName);
+        e_lname = findViewById(R.id.LastName);
+    }
+
+    public void regfunc(View view) {
+        final String fname = e_fname.getText().toString();
+        if (fname.equals("")) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Please enter First name", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidtermproject/registrationform2.java
@@ -0,0 +1,6 @@
+package com.example.androidtermproject;
+
+import android.app.Activity;
+
+public class registrationform2 extends Activity {
+}
