79b05cb99708c5534d98e5c0aefc4dd513027d36
Kiran Joseph <kiran.joseph.maliekal@gmail.com>
2020-04-13 21:49:37 -0400

    Adding OnclickListener to ListView

--- app/src/main/java/com/example/androidtermproject/EmployeeDetail.java
+++ app/src/main/java/com/example/androidtermproject/EmployeeDetail.java
@@ -3,6 +3,7 @@ package com.example.androidtermproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class EmployeeDetail extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class EmployeeDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_detail);
+        int employeeId=getIntent().getExtras().getInt("ID");
+        Toast.makeText(this,""+employeeId,Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/androidtermproject/MainActivity.java
+++ app/src/main/java/com/example/androidtermproject/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.androidtermproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidtermproject.business.IDataService;
 import com.example.androidtermproject.business.MemDataStoreSingleton;
+import com.example.androidtermproject.models.Employee;
+import com.example.androidtermproject.models.IEmployee;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private final String APP_NAME = "Hello";
@@ -23,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         dataService = MemDataStoreSingleton.getInstance();
         ListAdapter listAdapter = new ListAdapter(this, dataService.getEmployees());
         listview.setAdapter(listAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ArrayList <IEmployee> employees=dataService.getEmployees();
+                Employee e=(Employee) employees.get(position);
+                Intent intent=new Intent(MainActivity.this,EmployeeDetail.class);
+                intent.putExtra("ID",e.getId());
+                startActivity(intent);
+            }
+        });
     }
 
     public void goToReg(View view) {
